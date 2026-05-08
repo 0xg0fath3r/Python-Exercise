3fe693173b27a8e77a8f3a4c81b4a80e05093e07
0xg0fath3r <166716520+0xg0fath3r@users.noreply.github.com>
2026-05-08 11:09:57 +0100

    Create beginner-excersices.py

---
+++ beginner-excersices.py
@@ -0,0 +1,20 @@
+#1. How to calculate the hypothenus of a triangle.
+
+adjacent_a = int(input("Enter the adjacent side: "))
+obtuse_b = int(input("Enter the obtuse side: "))
+
+hypothenus_c = (adjacent_a**2 + obtuse_b**2)**0.5 # multiplied by 0.5 since root of a number can also be that number multiplied by 0.5
+
+print(hypothenus_c)
+
+#2. Currency exchange: multiply the currency by the current exchange price in dollar
+
+CO = int(input("What do you ahve left in pesos? ")) * 0.00027
+PE = int(input("What do you ahve left in soles? ")) * 0.29
+BR = int(input("What do you ahve left in reais? ")) * 0.20
+
+total_in_usd = CO + PE + BR
+
+print(total_in_usd)
+
+
